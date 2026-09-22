04f7f7aace41551ad471befef9c3816bf31bb438
wakawa2003 <48667049+wakawa2003@users.noreply.github.com>
2026-09-22 17:56:15 +0700

    update

--- Assets/AAAAA New GamePlay/Scripts/NewEnemyController.cs
+++ Assets/AAAAA New GamePlay/Scripts/NewEnemyController.cs
@@ -4,10 +4,12 @@ using Aiara;
 using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Triggers;
 using DG.Tweening;
+using MoreMountains.Feedbacks;
 using R3;
 using UniState;
 using Unity.Mathematics;
 using UnityEngine;
+using Yeolha.BeltScroll;
 
 
 namespace MyGameNamespace
@@ -25,10 +27,10 @@ namespace MyGameNamespace
         [SerializeField] private float attackTime = 1;
         [SerializeField] private float attackTimeFeedback = 0.2f;
         [SerializeField] private float attackDelay = 0.2f;
-        [SerializeField] private float attackRangeClose = 2.2f;
-        [SerializeField] private float attackRangeFar = 5.2f;
         public int Health = 100;
         public int MaxHealth = 100;
+        [SerializeField] private AttackingState.CloseAttack CloseAttack;
+        [SerializeField] private AttackingState.CloseAttack FarAttack;
 
         NewCharacterController target;
         IStateMachine stateMachine = new StateMachine();
@@ -48,12 +50,7 @@ namespace MyGameNamespace
             stateMachine = new StateMachine();
             Health = MaxHealth;
             stateMachine.SetResolver(new Resolver());
-            stateMachine.Execute<NormalState, NewEnemyController>(this, destroyCancellationToken);
-        }
-
-        void Start()
-        {
-            FeedbackManager.PlayFeedbackAtWorld("Enemy_Landing", transform.position, transform.rotation);
+            stateMachine.Execute<IntroState, NewEnemyController>(this, destroyCancellationToken);
         }
 
         public bool IsDeath() => Health <= 0;
@@ -63,8 +60,17 @@ namespace MyGameNamespace
             animator.SetBool("Walking", false);
             animator.SetFloat("RelativeForwardSpeedNormalized", 0);
         }
-
-
+        public class IntroState : StateBase<NewEnemyController>
+        {
+            public async override UniTask<StateTransitionInfo> Execute(CancellationToken token)
+            {
+                FeedbackManager.PlayFeedbackAtWorld("Enemy_Prepare_Landing", Payload.transform.position, Payload.transform.rotation);
+                await UniTask.WaitForSeconds(1);
+                FeedbackManager.PlayFeedbackAtWorld("Enemy_Landing", Payload.transform.position, Payload.transform.rotation);
+                await UniTask.WaitForSeconds(2);
+                return Transition.GoTo<NormalState, NewEnemyController>(Payload);
+            }
+        }
         public class NormalState : StateBase<NewEnemyController>
         {
             private IDisposable moveSubscription;
@@ -80,6 +86,8 @@ namespace MyGameNamespace
             }
             public override async UniTask<StateTransitionInfo> Execute(CancellationToken token)
             {
+
+                bool isCanAttack = false;
                 moveSubscription?.Dispose();
                 moveSubscription = Observable
                     .EveryUpdate(UnityFrameProvider.FixedUpdate)
@@ -90,14 +98,16 @@ namespace MyGameNamespace
                         float x = 0;
                         float y = 0;
 
-                        var attackRangeMin = Mathf.Min(Payload.attackRangeFar, Payload.attackRangeFar);
-                        var attackRangeMax = Mathf.Min(Payload.attackRangeFar, Payload.attackRangeFar);
+                        var attackRangeMin = Mathf.Min(Payload.CloseAttack.range, Payload.FarAttack.range);
+                        var attackRangeMax = Mathf.Min(Payload.CloseAttack.range, Payload.FarAttack.range);
 
-                        if (Vector3.Distance(Payload.transform.position, Payload.target.transform.position) >= attackRangeMax)
+                        if (Vector3.Distance(Payload.transform.position, Payload.target.transform.position) >= attackRangeMax)//move toi tam co the danh
                         {
                             x = -math.sign(Payload.transform.position.x - Payload.target.transform.position.x);
                         }
 
+                        else
+                            isCanAttack = true;
 
                         if (x != 0 && Payload.body != null)
                         {
@@ -106,6 +116,9 @@ namespace MyGameNamespace
                             Payload.body.localScale = scale;
                             Payload.animator.SetFloat("RelativeForwardSpeedNormalized", math.sign(scale.x) * x);
                         }
+
+                        // y = math.sign(Payload.transform.position.y - Payload.target.transform.position.y);
+
                         Payload.animator.SetBool("Walking", x != 0 || y != 0);
                         Payload.animator.SetFloat(
                             "WalkSpeedMultiplier",
@@ -126,12 +139,16 @@ namespace MyGameNamespace
                     await Observable
                         .EveryUpdate(UnityFrameProvider.Update)
                         .FirstAsync(_ =>
-                            Payload.IsDeath() ||
+                            Payload.IsDeath() || isCanAttack ||
                             Input.GetButtonDown("Fire1"));
 
                     // Ưu tiên chết nếu cùng frame vừa attack vừa hết máu.
                     if (Payload.IsDeath())
                         return Transition.GoTo<DeathState, NewEnemyController>(Payload);
+
+                    //attack
+                    if (isCanAttack)
+                        return Transition.GoTo<AttackingState, NewEnemyController>(Payload);
 
                     return Transition.GoTo<AttackingState, NewEnemyController>(Payload);
                 }
@@ -153,25 +170,47 @@ namespace MyGameNamespace
 
             public override async UniTask<StateTransitionInfo> Execute(CancellationToken token)
             {
-                Payload.animator.SetTrigger("Attack");
-                Payload.animator.SetInteger("AttackStage", Payload.attackStage);
 
-
-                DOVirtual.DelayedCall(Payload.attackTimeFeedback, delegate
-                        {
-                            FeedbackManager.PlayFeedbackAtWorld("Character Attack", Payload.transform.position, Payload.transform.rotation);
-                        });
-                // Attack kéo dài 1 giây, nhưng chết là đổi state ngay.
-                float endTime = Time.time + Payload.attackTime;
-
-                await UniTask.WaitUntil(
-                    () => Payload.IsDeath() || Time.time >= endTime,
-                    cancellationToken: token);
-
-                if (Payload.IsDeath())
-                    return Transition.GoTo<DeathState, NewEnemyController>(Payload);
+                //danh gan
+                if (Vector3.Distance(Payload.transform.position, Payload.target.transform.position) <= Payload.CloseAttack.range)//move toi tam co the danh
+                {
+                    await Payload.CloseAttack.AttackAction();
+                }
+                //danh xa
+                else if (Vector3.Distance(Payload.transform.position, Payload.target.transform.position) <= Payload.FarAttack.range)//move toi tam co the danh
+                {
+                    await Payload.FarAttack.AttackAction();
+                }
 
                 return Transition.GoTo<NormalState, NewEnemyController>(Payload);
+            }
+
+
+            [System.Serializable]
+            public class CloseAttack
+            {
+                public float range = 3;
+                public float waitTime = 1f;
+                [SerializeField] private MMF_Player MMF_PlayerFeedBack;
+                public async UniTask AttackAction()
+                {
+                    await MMF_PlayerFeedBack.PlayFeedbacksTask();
+                    await UniTask.WaitForSeconds(waitTime);
+                }
+            }
+
+            [System.Serializable]
+            public class FarAttack
+            {
+                public float range = 6;
+                public float waitTime = 0.5f;
+
+                [SerializeField] private MMF_Player MMF_PlayerFeedBack;
+                public async UniTask AttackAction()
+                {
+                    await MMF_PlayerFeedBack.PlayFeedbacksTask();
+                    await UniTask.WaitForSeconds(waitTime);
+                }
             }
         }
 
